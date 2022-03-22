255e22bb3ecd4ceb363e3e25673c30aa790f661b
Steven Kazmierkiewicz <stevekaz@gmail.com>
2022-03-22 15:50:04 -0400

    Added method for showing feature correlation heatmap

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import pandas as pd
-import matplotlib as pyplot
+import matplotlib.pyplot as plt
 
-print("This is a test")
+
+def show_correlation_heatmap(df):
+    plt.imshow(df.corr())
+    plt.colorbar()
+    plt.xticks(range(len(df.columns)), df.columns, rotation='vertical')
+    plt.yticks(range(len(df.columns)), df.columns)
+    plt.show()
+
+
+datasource_loc = "creditcard.csv"
+df = pd.read_csv(datasource_loc)
+show_correlation_heatmap(df)
+
