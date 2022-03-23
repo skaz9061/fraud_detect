d399a0e8c7794418b970e0e45551850d0f1bb343
Steven Kazmierkiewicz <stevekaz@gmail.com>
2022-03-23 11:14:51 -0400

    Preliminary testing of Categorization models

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import metrics
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score, cross_validate, cross_val_predict, TimeSeriesSplit, ShuffleSplit
 
 
 def show_correlation_heatmap(df):
+    corrs_abs = abs(df.corr())
+
     plt.imshow(df.corr())
     plt.colorbar()
     plt.xticks(range(len(df.columns)), df.columns, rotation='vertical')
@@ -21,20 +26,41 @@ def show_metrics(y_true, y_pred):
 
 datasource_loc = "creditcard.csv"
 df = pd.read_csv(datasource_loc)
-#show_correlation_heatmap(df)
+show_correlation_heatmap(df)
 
 X = df.values[:, 0:30]
 Y = df.values[:, 30]
-clf = RandomForestClassifier(n_estimators=20)
-clf = clf.fit(X, Y)
+#clf = RandomForestClassifier(n_estimators=20)
+#clf = RandomForestClassifier(n_estimators=20, max_depth=3)
+#clf = RandomForestClassifier(n_estimators=20, max_depth=5)
+#clf = RandomForestClassifier(n_estimators=20, max_features=5, max_depth=5)
+#clf = RandomForestClassifier(n_estimators=20, max_features=3, max_depth=5)
+#clf = RandomForestClassifier(n_estimators=40, max_features=3, max_depth=5)
+#clf = RandomForestClassifier(n_estimators=100, max_features=3, max_depth=5)
+#clf = RandomForestClassifier(n_estimators=50, max_features=4, max_depth=5)
+#clf = RandomForestClassifier(n_estimators=20, max_features=5, max_depth=5, random_state=0)
+#clf = RandomForestClassifier(n_estimators=20, max_features='sqrt', max_depth=5, random_state=0)
+#clf = RandomForestClassifier(n_estimators=20, max_features='log2', max_depth=5, random_state=0)
+clf = RandomForestClassifier(n_estimators=20, max_features='auto', max_depth=5, random_state=0)
+#clf = LogisticRegression()
+#clf = GradientBoostingClassifier(n_estimators=20, learning_rate=1.0, max_depth=3, random_state=0)
+#clf = GradientBoostingClassifier(n_estimators=20)
+#clf = GradientBoostingClassifier(n_estimators=20, learning_rate=0.2, max_depth=3, random_state=0)
+#clf = GradientBoostingClassifier(n_estimators=20, learning_rate=0.5, max_features='sqrt', max_depth=5, random_state=0)
+#clf = clf.fit(X, Y)
 #print(clf.predict([[4462, -2.303349568, 1.75924746, -0.359744743, 2.330243051, -0.821628328, -0.075787571, 0.562319782,
 #                    -0.698826069, -2.282193829, -4.781830856, -2.615664945, -1.334441067, -0.430021867, -0.294166318,
 #                    -0.932391057, 0.172726296, -0.087329538, -0.156114265, -0.542627889, 0.039565989, -0.153028797,
 #                    239.93]]))
 
-Y_pred = clf.predict(X)
-show_metrics(Y, Y_pred)
+#Y_pred = clf.predict(X)
+#show_metrics(Y, Y_pred)
+#tss = TimeSeriesSplit(n_splits=5)
+ss = ShuffleSplit(n_splits=5, test_size=0.2)
 
+scores = cross_validate(clf, X, Y, cv=ss, scoring=['accuracy', 'recall', 'precision'])
+
+print(scores)
 
 
 
